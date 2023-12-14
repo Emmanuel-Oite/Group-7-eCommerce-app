328b73beeb30f86f52779d3fc19491071a513fa7
Emmanuel-Oite <143942455+Emmanuel-Oite@users.noreply.github.com>
2023-12-14 18:29:22 +0300

    Fixed compiling errors

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,72 +1,106 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import axios from 'axios'; // Ensure axios is imported if you're using it for HTTP requests
+import { useCart } from '../contexts/CartContext';
+import { useAuth } from '../contexts/AuthContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCreditCard, faMoneyBill } from '@fortawesome/free-solid-svg-icons';
+import { faPaypal as fabPaypal } from '@fortawesome/free-brands-svg-icons';
+import '../index.css';
 
-function Checkout({ cartItems }) {
-  const [customerInfo, setCustomerInfo] = useState({
-    name: '',
-    email: '',
-    address: '',
-    paymentMethod: 'Credit Card'
+const Checkout = () => {
+  const { user } = useAuth();
+  const { cart, dispatch } = useCart();
+  
+  const [paymentOption, setPaymentOption] = useState('');
+  const [address, setAddress] = useState({
+    location: '',
+    phone: '',
+    // Add more address fields as needed
   });
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setCustomerInfo({ ...customerInfo, [name]: value });
+  const handlePaymentOption = (option) => {
+    setPaymentOption(option);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Logic to process the checkout
-    console.log('Checkout details:', customerInfo, cartItems);
+  const handleAddressChange = (e) => {
+    const { name, value } = e.target;
+    setAddress((prevAddress) => ({ ...prevAddress, [name]: value }));
   };
+
+  const handleCheckout = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/api/checkout', {
+        userId: user.id,
+        items: cart
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${user.token}`
+        }
+      });
+
+      alert('Checkout successful! Order ID: ' + response.data.orderId);
+    } catch (error) {
+      console.error('Checkout failed:', error);
+      alert('Checkout failed: ' + (error.response?.data?.message || 'Please try again.'));
+    }
+  };
+
+  useEffect(() => {
+    if (!user) {
+      alert('Please login or sign up to proceed with the purchase.');
+    }
+
+    if (paymentOption === 'card') {
+      window.location.href = 'https://mea.mastercard.com/en-region-mea.html';
+    } else if (paymentOption === 'cash') {
+      window.location.href = 'https://codshopy.com/';
+    } else if (paymentOption === 'paypal') {
+      window.location.href = 'https://www.paypal.com/ke/home';
+    }
+  }, [paymentOption, user]);
 
   return (
-    <div>
-      <h2>Checkout</h2>
-      <div>
-        <h3>Your Cart</h3>
-        {cartItems.map((item, index) => (
-          <div key={index}>
-            <p>{item.name} - ${item.price}</p>
-          </div>
-        ))}
+    <div className="checkout-container">
+      <h2 className="text-2xl font-semibold mb-4">Checkout</h2>
+      <div className="payment-options">
+        <div className={`option ${paymentOption === 'card' ? 'selected' : ''}`} onClick={() => handlePaymentOption('card')}>
+          <FontAwesomeIcon icon={faCreditCard} />
+          <span>Credit Card</span>
+        </div>
+        <div className={`option ${paymentOption === 'cash' ? 'selected' : ''}`} onClick={() => handlePaymentOption('cash')}>
+          <FontAwesomeIcon icon={faMoneyBill} />
+          <span>Cash on Delivery</span>
+        </div>
+        <div className={`option ${paymentOption === 'paypal' ? 'selected' : ''}`} onClick={() => handlePaymentOption('paypal')}>
+          <FontAwesomeIcon icon={fabPaypal} />
+          <span>PayPal</span>
+        </div>
       </div>
-      <form onSubmit={handleSubmit}>
+      <div className="address-section">
+        <h3 className="text-lg font-semibold mb-2">Address Information</h3>
+        <label className="label">Location:</label>
         <input
           type="text"
-          name="name"
-          placeholder="Your Name"
-          value={customerInfo.name}
-          onChange={handleInputChange}
-          required
+          name="location"
+          value={address.location}
+          onChange={handleAddressChange}
+          className="input-field"
         />
+        <label className="label">Phone Number:</label>
         <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={customerInfo.email}
-          onChange={handleInputChange}
-          required
+          type="text"
+          name="phone"
+          value={address.phone}
+          onChange={handleAddressChange}
+          className="input-field"
         />
-        <textarea
-          name="address"
-          placeholder="Shipping Address"
-          value={customerInfo.address}
-          onChange={handleInputChange}
-          required
-        />
-        <select
-          name="paymentMethod"
-          value={customerInfo.paymentMethod}
-          onChange={handleInputChange}
-        >
-          <option value="Credit Card">Credit Card</option>
-          <option value="PayPal">PayPal</option>
-          <option value="Cash on Delivery">Cash on Delivery</option>
-        </select>
-        <button type="submit">Complete Purchase</button>
-      </form>
+      </div>
+      <button onClick={handleCheckout} className="checkout-button">
+        Checkout
+      </button>
     </div>
   );
-}
+};
 
 export default Checkout;
