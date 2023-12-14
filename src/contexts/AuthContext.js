360378809e78624795227682ac5e8e02cd9ec0af
Bonventure Maeta Oanda <64786006+ADMBonny@users.noreply.github.com.>
2023-12-14 15:47:48 +0300

    Made some more changes to AuthContext file.

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -2,3 +2,10 @@
 import React, { createContext, useContext, useState } from 'react';
 
 const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+  
+    const login = (username) => {
+      setUser({ username });
+    };
